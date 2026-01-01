2f0e5bc8b70f9578fbb4daa962741b171768d820
Kunal Kundnani <kunalkundnani726@gmail.com>
2026-01-01 18:56:56 +0530

    committed

---
+++ JS Project/script.js
@@ -0,0 +1,13 @@
+let board = document.querySelector('.board');
+const blockWidth = 30
+const blockHeight = 30
+const cols = Math.floor(board.clientWidth / blockWidth)
+const rows = Math.floor(board.clientHeight / blockHeight)
+
+for(let row = 0 ; row<rows ; row++){
+    for(let col = 0; col < cols ; col++){
+        const block = document.createElement('div')
+        block.classList.add('box')
+        board.appendChild(block);
+    }
+}
